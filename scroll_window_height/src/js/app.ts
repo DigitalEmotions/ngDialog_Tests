60ef7e08be1f400a2e13055e395be10d1892a81c
Robert Silverton <rob.silverton@gmail.com>
2016-06-23 11:25:44 +1200

    scroll_window_height

---
+++ scroll_window_height/src/js/app.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../defs/angular.d.ts" />
+/// <reference path="../../defs/angular-ui-router.d.ts" />
+
+angular.module('app', [
+			   'templates',
+			   'ui.router',
+			   'app.controllers',
+			   'ngDialog'
+])
+
+angular.module('app.controllers', []);
